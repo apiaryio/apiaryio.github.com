65b503e92157ce3d20525a1c316e3ec63f826509
Jakub Mikulas <jakub@mikul.as>
2018-09-19 15:13:48 +0200

    chore: test git config

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,9 @@ git config --global user.name "Apiary Bot"
 git config --global user.email "support@apiary.io"
 git config --global user.name
 git config --global user.email
-
 echo "Setup git remote"
 git remote set-url origin "https://${GH_TOKEN}@github.com/apiaryio/apiaryio.github.com"
 
 echo "Run rake deploy"
+git config --global -l
 bundle exec rake deploy
