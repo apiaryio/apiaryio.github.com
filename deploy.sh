375ab0ad5fe7140b0e6ab359151650adceb191fa
Jakub Mikulas <jakub@mikul.as>
2018-09-19 16:37:02 +0200

    chore: rewrite from rake to shell

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,35 @@
-echo "Setup local git"
-git config --local user.name "Apiary Bot"
-git config --local user.email "support@apiary.io"
-echo "Setup git remote"
-git remote set-url origin "https://${GH_TOKEN}@github.com/apiaryio/apiaryio.github.com"
+echo "Setup global git"
+git config --global user.name "Apiary Bot"
+git config --global user.email "support@apiary.io"
 
-echo "Run rake deploy"
-bundle exec rake deploy
+echo "\n"
+echo "Setup git remote"
+REMOTE="https://${GH_TOKEN}@github.com/apiaryio/apiaryio.github.com"
+git remote set-url origin $REMOTE
+
+echo "\n"
+echo "Clone gh-pages"
+rm -rf _gh-pages
+git clone $REMOTE _gh-pages
+
+cd _gh-pages
+git checkout gh-pages
+git reset --hard
+git clean -xdf
+
+echo "\n"
+echo "Run build"
+cd ..
+chmod -R 777 _gh-pages
+bundle exec jekyll build -d _gh-pages
+
+cd _gh-pages
+
+echo "\n"
+echo "Cleanup and push"
+git checkout gh-pages
+git pull origin gh-pages
+git ls-files --deleted -z | xargs -0 git rm;
+git add .
+git commit -m "Deploy"
+git push
