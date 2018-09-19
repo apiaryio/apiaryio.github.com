c3f517ea5484420a0494aa0238c46fad7d7939b0
Jakub Mikulas <jakub@mikul.as>
2018-09-19 15:35:45 +0200

    chore: use git —local config

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,8 @@
 echo "Setup local git"
-git config --global user.name "Apiary Bot"
-git config --global user.email "support@apiary.io"
-git config --global user.name
-git config --global user.email
+git config --local user.name "Apiary Bot"
+git config --local user.email "support@apiary.io"
 echo "Setup git remote"
 git remote set-url origin "https://${GH_TOKEN}@github.com/apiaryio/apiaryio.github.com"
 
 echo "Run rake deploy"
-git config --global -l
-git config --local -l
 bundle exec rake deploy
